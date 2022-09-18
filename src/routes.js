b592347f31cde271b08925fa26b15a402e3f7172
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-09-18 14:46:58 +0300

    Add about page

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
 const homeController = require('./controllers/homeController');
+const cubeController = require('./controllers/cubeController');
 
 const router = express.Router();
 
 router.get('/', homeController.index);
-
+router.get('/about', homeController.about);
+router.use('/cube', cubeController)
 module.exports = router;
