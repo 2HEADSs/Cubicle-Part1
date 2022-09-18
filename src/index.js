608fd47225f634a2d2d8fcff5b8e94d590bfbaca
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-09-18 13:40:29 +0300

    Move public to root folder

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const handlebars = require('express-handlebars')
 
 const app = express();
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static('public'));
 
-app.get('/',(req, res)=>{
-    res.send('Hello World!')
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs'
+}));
+
+app.set('view engine', 'hbs');
+app.set('views', './src/views');
+
+app.get('/', (req, res) => {
+    res.render('index')
 });
 
-app.listen(5000, ()=> console.log(`App is listening on port 5000...`))
+app.listen(5000, () => console.log(`App is listening on port 5000...`))
