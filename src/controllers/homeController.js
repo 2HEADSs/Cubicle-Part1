6b105780b29f8a8dae572e73c15f013f44776cde
Pavel.Dimitrov <2heads.pd@gmail.com>
2022-09-20 12:50:49 +0300

    Add search

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
-const cubes = require('../db.json');
+const cubeService = require('../services/cubeService');
+
 
 router.get('/', (req, res) => {
+    let { search, from, to } = req.query;
+
+
+    const cubes = cubeService.getAll(search, from, to);
+
     res.render('index', { cubes })
 });
 
@@ -9,7 +15,7 @@ router.get('/about', (req, res) => {
     res.render('about')
 });
 
-router.post('/pesho', (req, res) => { 
+router.post('/pesho', (req, res) => {
     console.log(req.body);
     res.send('Successful form submit!')
 })
--- src/services/cubeService.js
+++ src/services/cubeService.js
@@ -1,13 +1,25 @@
 const fs = require('fs/promises');
+const path = require('path');
+
 const cubes = require('../db.json');
 
-//cubes[cubeId] - array[0] - array[index] - is not an object
-exports.getOne = (cubeId) => cubes[cubeId]
+exports.getAll = (search = '', fromInput, toInput) => {
+    const from = Number(fromInput) || 0;
+    const to = Number(toInput) || 6;
+    
+    const result = cubes
+        .filter(x => x.name.toLowerCase().includes(search.toLowerCase()))
+        .filter(x => x.difficultyLevel >= from && x.difficultyLevel <= to);
 
+    return result;
+};
+
+exports.getOne = (cubeId) => cubes.find(x => x.id === Number(cubeId));
 
 exports.save = (cube) => {
-    cubes.push(cube);
-    let textData = JSON.stringify(cubes, '', 4)
-    return fs.writeFile('./src/db.json', textData, { encoding: 'utf-8' })
+    cubes.push({ id: cubes[cubes.length - 1].id + 1, ...cube });
 
+    let textData = JSON.stringify(cubes, '', 4);
+
+    return fs.writeFile(path.resolve('src', 'db.json'), textData, { encoding: 'utf-8' })
 }
